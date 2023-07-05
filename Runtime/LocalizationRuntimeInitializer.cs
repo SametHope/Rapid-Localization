9e5ba85097c00c4154aa5430701924fa98d8b7f1
SametHope <samethgoxx@gmail.com>
2023-07-05 15:02:44 +0300

    Runtime initializer now considers domain reloads

--- Runtime/LocalizationRuntimeInitializer.cs
+++ Runtime/LocalizationRuntimeInitializer.cs
@@ -5,7 +5,21 @@ namespace SametHope.RapidLocalization
     public static class LocalizationRuntimeInitializer
     {
         /// <summary>
-        /// This method (and class) makes it so the localization will initialize itself without need of any manual call or gameobject and remember the last sessions language.
+        /// This method will clear the dictionary if it was not cleaned by the domain reloading automatically.
+        /// This is handy when enter play mode options are being used.
+        /// </summary>
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        public static void CleanDictionaryIfNeeded()
+        {
+            if (LocalizationManager.Dictionary.Count > 0)
+            {
+                LocalizationManager.Dictionary.Clear();
+            }
+        }
+
+        /// <summary>
+        /// This method makes it so the localization will initialize itself 
+        /// without need of any manual call or gameobject and remember the last sessions language.
         /// </summary>
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static void Initialize()
